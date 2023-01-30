c5a00ffa213b16a2c3ad13d67799cf3799cba4c6
Jérémy Sorant <jsorant@gmail.com>
2023-01-30 23:23:50 +0100

    Added a scenario

--- tests/StepsDefinitions/Steps.ts
+++ tests/StepsDefinitions/Steps.ts
@@ -13,8 +13,9 @@ import { ProjectionsPersistence } from "../../src/App/Queries/Ports/ProjectionsP
 import { VehicleProjection } from "../../src/App/Queries/Views/VehicleProjection";
 
 let dataPersistence: FleetRepository & ProjectionsPersistence;
-let myFleetId: string;
-let vehiclePlate: string;
+const myFleetId: string = "my_fleet_id";
+const anotherUserFleetId: string = "another_user_fleet_id";
+const vehiclePlate: string = "vehicle_plate";
 let lastError: any | undefined;
 
 Before(async function () {
@@ -23,14 +24,11 @@ Before(async function () {
 });
 
 Given("my fleet", async function () {
-  myFleetId = "my_fleet_id";
-  const command: CreateFleet = new CreateFleet(myFleetId);
-  const handler: CreateFleetHandler = new CreateFleetHandler(dataPersistence);
-  await handler.execute(command);
+  await createFleet(myFleetId);
 });
 
 Given("a vehicle", function () {
-  vehiclePlate = "vehicle_plate";
+  // Defined by 'vehiclePlate'
 });
 
 When("I register this vehicle into my fleet", async function () {
@@ -72,12 +70,37 @@ Then(
   }
 );
 
+Given("the fleet of another user", async function () {
+  await createFleet(anotherUserFleetId);
+});
+
+Given(
+  "this vehicle has been registered into the other user's fleet",
+  async function () {
+    await registerVehicleIntoAnotherUserFleet();
+  }
+);
+
 function resetPersistenceLayer(): void {
   dataPersistence = new InMemoryDataPersistence();
 }
 
+async function createFleet(fleetId: string) {
+  const command: CreateFleet = new CreateFleet(fleetId);
+  const handler: CreateFleetHandler = new CreateFleetHandler(dataPersistence);
+  await handler.execute(command);
+}
+
 async function registerVehicleIntoMyFleet(): Promise<void> {
-  const command: RegisterVehicle = new RegisterVehicle(vehiclePlate, myFleetId);
+  await registerVehicleInto(myFleetId);
+}
+
+async function registerVehicleIntoAnotherUserFleet(): Promise<void> {
+  await registerVehicleInto(anotherUserFleetId);
+}
+
+async function registerVehicleInto(fleetId: string): Promise<void> {
+  const command: RegisterVehicle = new RegisterVehicle(vehiclePlate, fleetId);
   const handler: RegisterVehicleHandler = new RegisterVehicleHandler(
     dataPersistence
   );
